3902b7c966b1ce09d3b76b5a64705eb1f64f7268
纵昂 <zongang15@yahoo.com>
2021-03-22 15:20:36 +0800

    Initial Commit

---
+++ HuaHua/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HuaHua
+//
+//  Created by 纵昂 on 2021/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HuaHua/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HuaHua
+//
+//  Created by 纵昂 on 2021/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
